a01115fc49807f742ae29ecec048332d6f1a388d
Matteo Bachetti <matteo@matteobachetti.it>
2025-07-09 22:02:53 +0200

    Add tests for get_crab_ephemeris

---
+++ toa_extractor/utils/tests/test_crab.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+from toa_extractor.utils.crab import get_crab_ephemeris
+
+
+@pytest.mark.parametrize("ephem", ["DE200", "DE430", "DE440"])
+def test_refit_solution_ephem(ephem):
+    mjd = 57974.71650905952  # Problematic in some previous tests
+    model = get_crab_ephemeris(mjd, ephem=ephem, force_parameters=None)
+
+    if ephem.upper() == "DE200":
+        assert model.TRES.value is None
+    else:
+        assert model.TRES.value < 1
+
+
+@pytest.mark.parametrize("mjd", np.random.uniform(44000, 70000, 5))
+def test_refit_solution_mjd(mjd):
+    model = get_crab_ephemeris(mjd, ephem="DE430", force_parameters=None)
+    assert model.TRES.value < 1
